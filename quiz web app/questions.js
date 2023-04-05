34c0ca7f368c340c5416c920ff5d447c19d2df18
Shruti Raj Sharma <79409619+shrutirajsharma@users.noreply.github.com>
2023-04-05 22:17:46 +0530

    Add files via upload

---
+++ quiz web app/questions.js
@@ -0,0 +1,96 @@
+// www.bytewebster.com
+// www.bytewebster.com
+
+let questions = [
+    {
+    numb: 1,
+    question: "Prime Minister of India?",
+    answer: "Narendra Modi",
+    options: [
+      "Rahul Gandhi",
+      "Narendra Modi",
+      "Kejriwal",
+      "Lalu Yadav"
+    ]
+  },
+    {
+    numb: 2,
+    question: "National Bird of India?",
+    answer: "Peacock",
+    options: [
+      "Ostrich",
+      "Peacock",
+      "Parrot",
+      "Crow"
+    ]
+  },
+    {
+    numb: 3,
+    question: "National Animal of India?",
+    answer: "Tiger",
+    options: [
+      "Tiger",
+      "Lion",
+      "Elephant",
+      "Giraffe"
+    ]
+  },
+    {
+    numb: 4,
+    question: "National Sports of India?",
+    answer: "Hockey",
+    options: [
+      "Hockey",
+      "Cricket",
+      "Badminton",
+      "Volley Ball"
+    ]
+  },
+    {
+    numb: 5,
+    question: " National fruit of India?",
+    answer: "Mango",
+    options: [
+      "Mango",
+      "Apple",
+      "Orange",
+      "Grapes"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "WWW full form?",
+    answer: "World Wide Web",
+    options: [
+      "World Wide Web",
+      "World Web Wide",
+      "Web Wide World",
+      "None of the above"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Cascading Style Sheet",
+      "Create Service Sheet",
+      "Cascading Sheet Style",
+      "Content Sheet Service"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What does SEO stand for?",
+    answer: "Searh Engine Optimization",
+    options: [
+      "Stylish Engine Object",
+      "Searh Engine Optimization",
+      "Statement Engine Optimization",
+      "Searh Environment Object"
+    ]
+  },
+];
